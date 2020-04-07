91e8cdbd2c9db4693bc48bb788cb5aa6135aecbb
Shriram Rao <srao2207@gmail.com>
2020-04-08 01:10:30 +0530

    updated the changes,still working on js

--- js/project.js
+++ js/project.js
@@ -5,33 +5,31 @@
     const personHeight=document.getElementById('height');
     const personGender=document.getElementById('gender');
     const personSkin_color=document.getElementById('skin');
+    const choice=document.getElementById('person').value;
+    
     const nextPersonBtn = document.getElementById('btn2');
-
     const prevPersonBtn=document.getElementById('btn1');
     
-    const swapiPeopleAPI = (index = 1) => `https://swapi.co/api/people/${index}/?format=json`;
-    
+    const swapiPeopleAPI = (index = 1) => `https://swapi.co/api/${choice}/${index}/?format=json`;
+  
     function getPerson(index = 1) {
       return fetch(swapiPeopleAPI(index)).then(data => data.json());
     }
     
+    /* next button code */
+
     async function updateNextPerson() {
-      const {name} = await getPerson(++index);
-      const {height}=await getPerson(index);
-      const {gender}=await getPerson(index);
-      const {skin_color}=await getPerson(index);
+      const {name,height,gender,skin_color} = await getPerson(++index);
       peronName.innerText = name;
       personHeight.innerText=height;
       personGender.innerText=gender;
       personSkin_color.innerText=skin_color;     
     }
     
-    
-    async function updateNextPerson1() {
-        const {name} = await getPerson(--index);
-        const {height} = await getPerson(index);
-        const {gender} = await getPerson(index);
-        const {skin_color} = await getPerson(index);
+    /*this is previous button part */
+
+    async function updatePreviousPerson1() {
+        const {name,height,gender,skin_color} = await getPerson(--index);
         peronName.innerText = name;
         personHeight.innerText=height;
         personGender.innerText=gender;
@@ -39,7 +37,7 @@
 
       }
       prevPersonBtn.addEventListener('click',function(e){
-        updateNextPerson1();
+        updatePreviousPerson1();
     },false)
 
       
