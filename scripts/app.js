ceb6bc28e5f2071d28a313af62c30d755c50ac4e
MithunBhat <mithun.r.bhat99@gmail.com>
2020-04-09 00:42:13 +0530

    progress

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,16 @@
-((window,document,undefined) => {
+(() => {
 
     var index = 1;
+    var people = [];
+    var planets = [];
+    var species = [];
+    // var category = "";
 
     //category-to-select
-    const people = document.getElementById("people");
-    const planet = document.getElementById("planets");
+    const peopleBtn = document.getElementById("people");
+    const planetBtn = document.getElementById("planets");
     const spaceship = document.getElementById("spaceships");
-    const species = document.getElementById("species");
+    const speciesBtn = document.getElementById("species");
 
     //display-box__LHS-content
     let argone = document.getElementById("lhs-one");
@@ -37,40 +41,96 @@
     //     [...{ lableref: argsix }, ...{ valueRef: valuesix }]
     // ];
 
-    // const arrppl = ["name" ,"height", "hair_color", "skin_color", "birth_year", "gender"]
+    const arrppl = ["name" ,"height", "hair_color", "skin_color", "birth_year", "gender"];
+    const arrplnt = ["name","diameter","climate","gravity","surface_water","population"];
+    const arrspc = ["name","classification","designation","average_height","average_lifespan","language"];
 
-    const swapipplAPI = (index=1) => `https://swapi.co/api/people/${index}/?format=json`;
+    var crrArr = [];
 
-    function getData(index = 1) {
-        return fetch(swapipplAPI(index)).then(data => data.json());
+
+    // var swAPI = (index=1) => `https://swapi.co/api/people/${index}/?format=json`;
+
+    // function getData(index = 1) {
+    //     return fetch(swAPI(index)).then(data => data.json());
+    // }
+
+    // function getNextData() {
+    //     return ++index;
+    // }
+
+    // function getPrevData() {
+    //     return --index;
+    // }
+
+    // async function displayData() {
+    //     const {name, height} = await getData(index);
+    //     argone.innerText = "people";
+    //     valueone.innerText = name;
+    //     argtwo.innerText = "height";
+    //     valuetwo.innerText = height;
+    // }
+
+    // peopleBtn.addEventListener('click', (e) => {
+    //     category = "people";
+    //     displayData();
+    // },false)
+
+    // planetBtn.addEventListener('click', (e) => {
+    //     category = "planet";
+    //     displayData();
+    // },false)
+
+    // speciesBtn.addEventListener('click', (e) => {
+    //     category = "species";
+    //     displayData();
+    // },false)
+
+    // nextbtn.addEventListener('click', (e) => {
+    //     displayData(getNextData());
+    // },false)
+
+    // prevbtn.addEventListener('click', (e) => {
+    //     displayData(getPrevData());
+    // },false)
+
+    // displayData();
+    function fetchJson() {
+        fetch("./mock.json")
+        .then((res) => res.json())
+        .then((data) => {
+            data.forEach((cat) => {
+               people = cat.people; 
+               planets = cat.planets;
+               species = cat.species;
+            });
+            })
+    }
+    console.log(people);
+
+    async function sepCategory() {
+        const {people,planets,species} = await fetchJson();
+        return [people, planets, species];
     }
 
-    function getNextData() {
-        return ++index;
-    }
-
-    function getPrevData() {
-        return --index;
-    }
+    var category = sepCategory();
+    var people = category[0];
+    var planets = category[1];
+    var species = category[2];
 
     async function displayData() {
-        const {name, height} = await getData(index);
-        argone.innerText = "people";
-        valueone.innerText = name;
-        argtwo.innerText = "height";
-        valuetwo.innerText = height;
+        // const {crrArr[0],crrArr[1],crrArr[2],crrArr[3],crrArr[4],crrArr[5]} = await people[index];
+        const {name,height} = await people[index];
+        argone.innerText = "Name";
+        valueone.innerText = crrArr[0];
+        argtwo.innerText = "Height";
+        valuetwo.innerText = crrArr[1];
     }
 
-    people.addEventListener('click', (e) => {
+    peopleBtn.addEventListener('click', (e) => {
+        crrArr = arrppl;
         displayData();
-    },false)
+    })
 
-    nextbtn.addEventListener('click', (e) => {
-        displayData(getNextData());
-    },false)
+    displayData();
 
-    prevbtn.addEventListener('click', (e) => {
-        displayData(getPrevData());
-    },false)
-
-})(window,document);
+})();
