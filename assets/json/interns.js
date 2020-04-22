cd6bd9b484a070762e39780d791342cc2a22ae77
tejasra <56512902+tejasra@users.noreply.github.com>
2020-04-22 10:08:18 +0530

    Create interns.js

---
+++ assets/json/interns.js
@@ -0,0 +1,75 @@
+var data=[
+  {
+    "name": "Ranjith",
+    "groupName": "Mercury",
+    "groupId": "GP_01",
+    "id": "CC01"
+  },
+  {
+    "name": "Athish",
+    "groupName": "Mercury",
+    "groupId": "GP_01",
+    "id": "CC02"
+  },
+  {
+    "name": "Pragathi",
+    "groupName": "Mercury",
+    "groupId": "GP_01",
+    "id": "CC03"
+  },
+  {
+    "name": "Shriram",
+    "groupName": "Venus",
+    "groupId": "GP_02",
+    "id": "CC04"
+  },
+  {
+    "name": "Dhanalakshmi",
+    "groupName": "Venus",
+    "groupId": "GP_02",
+    "id": "CC05"
+  },
+  
+  {
+    "name": "Kiran",
+    "groupName": "Venus",
+    "groupId": "GP_02",
+    "id": "CC06"
+  },
+  {
+    "name": "Ashfan",
+    "groupName": "Earth",
+    "groupId": "GP_03",
+    "id": "CC07"
+  },
+  {
+    "name": "Mithun",
+    "groupName": "Earth",
+    "groupId": "GP_03",
+    "id": "CC08"
+  },
+  {
+    "name": "Abida",
+    "groupName": "Earth",
+    "groupId": "GP_03",
+    "id": "CC09"
+  },
+  {
+    "name": "Deekshith",
+    "groupName": "Mars",
+    "groupId": "GP_04",
+    "id": "CC010"
+  },
+  {
+    "name": "Tejas",
+    "groupName": "Mars",
+    "groupId": "GP_04",
+    "id": "CC011"
+  },
+  {
+    "name": "Mohinuddin",
+    "groupName": "Mars",
+    "groupId": "GP_04",
+    "id": "CC012"
+  }
+]
